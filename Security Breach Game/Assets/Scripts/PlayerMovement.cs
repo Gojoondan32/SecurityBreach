c1337951eaf9a0888a4e2547a9a9b6d004702878
RossH6348 <75311103+RossH6348@users.noreply.github.com>
2021-11-09 13:42:42 +0000

    Wall climbing prototype #1

--- Security Breach Game/Assets/Scripts/PlayerMovement.cs
+++ Security Breach Game/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 5f;
 
     [SerializeField]
+
+    private bool isClimbing = false;
+    public Camera viewCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,13 +28,44 @@ public class PlayerMovement : MonoBehaviour
 
         //Call the move player function calculate the direction need to move the player
         MovePlayer(x, z);
+
+        if (!isClimbing)
+            controller.Move(new Vector3(0.0f, -9.8f, 0.0f) * Time.deltaTime);
+
+        //Re-adjusting camera automatically.
+        Vector3 cameraOffset = new Vector3(0.0f, 10.0f, 0.0f);
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(gameObject.transform.position, cameraOffset.normalized, out hit, 10.0f))
+        {
+            cameraOffset = hit.point - gameObject.transform.position;
+        }
+
+        viewCamera.transform.position = gameObject.transform.position + cameraOffset;
+
     }
 
     private void MovePlayer(float x, float z)
     {
         Vector3 direction = new Vector3(x, 0, z);
+
+        if (isClimbing)
+            direction += new Vector3(0.0f, (x != 0.0f ? x : (z != 0.0f ? z : 0.0f)), 0.0f);
+
         Vector3 move = direction * speed;
 
         controller.Move(move * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Climbable"))
+            isClimbing = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Climbable"))
+            isClimbing = false;
     }
 }
