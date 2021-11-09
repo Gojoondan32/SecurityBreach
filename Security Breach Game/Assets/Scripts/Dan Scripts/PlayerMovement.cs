8d7905a1e888a3de60b592c828e9cd8eed036bbd
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2021-11-09 17:22:51 +0000

    Added the foundation for moving on walls

--- Security Breach Game/Assets/Scripts/Dan Scripts/PlayerMovement.cs
+++ Security Breach Game/Assets/Scripts/Dan Scripts/PlayerMovement.cs
@@ -10,12 +10,23 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float gravity = 1f;
 
+    [SerializeField] private float wallCheckDistance = 1f;
+
+    [SerializeField] private bool isOnGround;
+
+    [SerializeField] private bool isOnWall;
+
     private Vector3 velocity;
+
+    private Vector3 move;
     // Start is called before the first frame update
     void Start()
     {
         //Get the character controller
         controller = GetComponent<CharacterController>();
+
+        isOnGround = true;
+        isOnWall = false;
     }
 
     // Update is called once per frame
@@ -36,18 +47,50 @@ public class PlayerMovement : MonoBehaviour
     private void MovePlayer(float x, float z)
     {
         Vector3 direction = new Vector3(x, 0, z);
+        Vector3 wallDirection = new Vector3(x, z, 0);
 
-        Vector3 move = direction * speed;
+        move = direction * speed;
 
-        controller.Move(move * Time.deltaTime);
+        ChangeWall(move);
+        if (isOnGround)
+        {
+            controller.Move(move * Time.deltaTime);
+        }
+        else if (isOnWall)
+        {
+            controller.Move(wallDirection * speed * Time.deltaTime);
+        }
     }
 
 
-    private void AddGravity()
+    public void AddGravity()
     {
         //Change the velocity.y to change the focus of the gravity
-        velocity.y -= gravity * Time.deltaTime;
+        if (isOnGround)
+        {
+            velocity.y -= gravity * Time.deltaTime;
+        }
+        else if (isOnWall)
+        {
+            velocity.z += gravity * Time.deltaTime;
+            velocity.y = 0;
+        }
         controller.Move(velocity * Time.deltaTime);
     }
     
+    public void ChangeWall(Vector3 move)
+    {
+        if (isOnGround)
+        {
+            LayerMask wallMask = LayerMask.GetMask("Wall");
+            if(Physics.Raycast(transform.position, move.normalized, wallCheckDistance, wallMask))
+            {
+                isOnGround = false;
+                isOnWall = true;
+                //Rotate the player 
+                transform.rotation = Quaternion.Euler(270f, 0f, 0f);
+                Debug.Log("Player has hit a wall");
+            }
+        }
+    }
 }
