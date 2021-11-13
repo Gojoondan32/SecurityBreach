b0a742d66430fc3505ac69bc5d86b2e48e9bd1cf
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2021-11-13 23:17:44 +0000

    Basics of Metal Ore
    Added the Bare Minimum for the Metal Ore Script

---
+++ Security Breach Game/Assets/Scripts/Conrad Scripts/Metal Ore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalOre : MonoBehaviour
+{
+    private int metalCount;
+    public int metalTaken;
+
+    private void Start()
+    {
+        if (gameObject.name == "MetalOre")
+        {
+            metalCount = 20;
+        }
+        else if(gameObject.name == "Large Metal Ore")
+        {
+            metalCount = 40;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (metalTaken >= metalCount)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
