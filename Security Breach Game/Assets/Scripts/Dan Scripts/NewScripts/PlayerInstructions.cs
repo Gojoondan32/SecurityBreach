de84e81ac0c9aa6f9615914344613feb663486e9
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2021-11-13 15:36:29 +0000

    Added new player movement

---
+++ Security Breach Game/Assets/Scripts/Dan Scripts/NewScripts/PlayerInstructions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInstructions : MonoBehaviour
+{
+    [SerializeField] private float range = 5f;
+    private LayerMask botMask;
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        botMask = LayerMask.GetMask("Bots");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CallBots();
+        }
+    }
+
+    private void CallBots()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, botMask))
+        {
+            
+            //hit.point = Physics.OverlapSphere(hit.point, 5);
+            Collider[] botsHit = Physics.OverlapSphere(hit.point, range, botMask);
+
+            foreach (Collider bots in botsHit)
+            {
+                Debug.Log("Bots have been hit" + bots.name.ToString());
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(hit.point != null)
+        {
+            Gizmos.DrawWireSphere(hit.point, range);
+        }
+    }
+}
---
+++ Security Breach Game/Assets/Scripts/Dan Scripts/NewScripts/PlayerMove.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private CharacterController controller;
+    [SerializeField] private float speed = 5f;
+
+    
+
+    private float smoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    Vector3 moveDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        MovePlayer(x, z);
+    }
+
+    private void MovePlayer(float x, float z)
+    {
+        Vector3 direction = new Vector3(x, 0, z);
+        Vector3 move = direction * speed;
+
+        move = transform.TransformDirection(move);
+
+        //RotatePlayer(move);
+        
+
+        controller.Move(move * Time.deltaTime);
+
+
+
+        
+
+    }
+
+    private Vector3 RotatePlayer(Vector3 move)
+    {
+        float targetAngle = Mathf.Atan2(move.x, move.y) * Mathf.Rad2Deg;
+
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, smoothTime);
+
+        transform.rotation = Quaternion.Euler(0, angle, 0);
+
+        moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+        Debug.Log("Rotating the player");
+
+        return moveDir;
+    }
+}
