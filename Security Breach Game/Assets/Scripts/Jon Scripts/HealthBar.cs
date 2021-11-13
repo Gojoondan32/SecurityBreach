78c273a9f21262e2d649771ca6c2b5f10d547ff2
JonFieldhouse93 <75310581+JonFieldhouse93@users.noreply.github.com>
2021-11-13 15:21:37 +0000

    Added Bot Avatars, UI elements and early Health Bar script

---
+++ Security Breach Game/Assets/Scripts/Jon Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+public class HealthBar : UnityEngine.MonoBehaviour
+{
+    public Slider slider;
+
+    public UnityEngine.Gradient grad;
+
+    public Image fill;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        fill.color = grad.Evaluate(1f);
+
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        fill.color = grad.Evaluate(slider.normalizedValue);
+    }
+}
