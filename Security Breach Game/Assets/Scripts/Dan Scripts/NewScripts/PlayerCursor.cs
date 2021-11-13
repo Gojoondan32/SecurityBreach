f1058502432c7cafcc212f5717c22591946a1ca6
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2021-11-13 16:24:55 +0000

    Added cursor controls

---
+++ Security Breach Game/Assets/Scripts/Dan Scripts/NewScripts/PlayerCursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCursor : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Camera mainCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        
+        if(Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, layerMask))
+        {
+            transform.position = hit.point;
+        }
+        
+    }
+}
