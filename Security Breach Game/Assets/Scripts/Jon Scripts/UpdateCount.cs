de4a0a1592929727ca3aec7bd1ec13bf96422665
JonFieldhouse93 <75310581+JonFieldhouse93@users.noreply.github.com>
2021-11-14 13:02:00 +0000

    Started Bot Count Script

---
+++ Security Breach Game/Assets/Scripts/Jon Scripts/UpdateCount.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCount : MonoBehaviour
+{
+    public Text workercount;
+    public Text warriorcount;
+    public Text dronecount;
+    public Text popcount;
+    public Text orecount;
+
+    private int countWorker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countWorker = GameObject.FindGameObjectsWithTag("Worker").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        workercount.text = ((int)countWorker).ToString();
+    }
+}
