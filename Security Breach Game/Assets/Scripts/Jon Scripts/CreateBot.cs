9240c4ce7d2c07371ddfd87a5ed952895dccfefc
JonFieldhouse93 <75310581+JonFieldhouse93@users.noreply.github.com>
2021-11-15 16:01:01 +0000

    Implementing UI

---
+++ Security Breach Game/Assets/Scripts/Jon Scripts/CreateBot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateBot : MonoBehaviour
+{
+
+    public GameObject Worker;
+    public GameObject Warrior;
+    public GameObject Drone;
+
+    public Transform spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnWorker()
+    {
+        Instantiate(Worker, spawn);
+    }
+    public void spawnWarrior()
+    {
+        Instantiate(Warrior, spawn);
+    }
+    public void spawnDrone()
+    {
+        Instantiate(Drone, spawn);
+        Debug.Log("Drone spawned");
+    }
+
+}
